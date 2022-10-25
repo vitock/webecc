5f771de9ef8c4260699ad2d6c71d967d51f8072d
liw003 <liw003>
2022-10-25 11:23:23 +0800

    手动粘贴数据到邮箱，

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 (async function () {
   interface InputData {
-    prefix:string
-    pubkey:string
-    toEmail:string
-    emailSubject:string
+    prefix: string;
+    pubkey: string;
+    toEmail: string;
+    emailSubject: string;
   }
-  let G_Input : InputData;
+  let G_Input: InputData;
 
   let ec = await exports.initEC();
 
@@ -46,33 +46,33 @@
     let input = document.getElementById("plaintext") as HTMLTextAreaElement;
     return input?.value;
   }
- 
+
   function setErrMsg(str: string) {
-    console.log(str)
-    alert(str)
+    console.log(str);
+    alert(str);
   }
 
-  async function encryptClick (){
+  async function encryptClick() {
     console.log(getPublicKey());
     let p = getPublicKey();
     let text = getPlainText();
     if (!text) {
       setErrMsg("请输入明文");
-      return false
+      return false;
     }
     try {
       let te = new TextEncoder();
       let enc = await ec.encrypt(p, te.encode(text));
       setCipherText(ec.base64Encode(enc));
-      return true
+      return true;
     } catch (error) {
       setErrMsg(error as string);
       console.log(error);
-      return false
+      return false;
     }
   }
   document.getElementById("encrypt")!.onclick = async () => {
-    await encryptClick()
+    await encryptClick();
   };
 
   document.getElementById("decrypt")!.onclick = async () => {
@@ -126,80 +126,75 @@
   };
 
   document.getElementById("genpubkey")!.onclick = async () => {
-    let seckey = getPirvateKey()
-    console.log(seckey)
+    let seckey = getPirvateKey();
+    console.log(seckey);
     if (!seckey) {
-      setErrMsg("私钥为空")
-      return
+      setErrMsg("私钥为空");
+      return;
     }
     try {
       let kp = await ec.generateNewKeyPair(seckey);
       setPirvateKey(kp.private);
       setPublicKey(kp.public);
-    } catch (error:any) {
-      setErrMsg(error.toString())
+    } catch (error: any) {
+      setErrMsg(error.toString());
     }
-    
   };
 
+  async function pbkdf2(phrase: string) {
+    var substl = crypto.subtle;
 
-  async function pbkdf2(phrase:string){
+    let keyRaw = new TextEncoder().encode(phrase);
 
-  var substl = crypto.subtle
- 
-  let keyRaw = new TextEncoder().encode(phrase)
+    let key = await substl.importKey("raw", keyRaw, "PBKDF2", false, [
+      "deriveBits",
+    ]);
+    let salt =
+      "The California sea lion (Zalophus californianus) is a coastal species of eared seal native to western North America. It is one of six species of sea lion. Its natural habitat ranges from southeast Alaska to central Mexico, including the Gulf of California. This female sea lion was photographed next to a western gull in Scripps Park in the neighborhood of La Jolla in San Diego, California. [2022-04-07 wikipedia]";
 
-  let key = await substl.importKey('raw',keyRaw,'PBKDF2',false,["deriveBits"])
-  let salt = "The California sea lion (Zalophus californianus) is a coastal species of eared seal native to western North America. It is one of six species of sea lion. Its natural habitat ranges from southeast Alaska to central Mexico, including the Gulf of California. This female sea lion was photographed next to a western gull in Scripps Park in the neighborhood of La Jolla in San Diego, California. [2022-04-07 wikipedia]"
+    let pbkdf2 = {
+      name: "PBKDF2",
+      hash: "SHA-256",
+      iterations: 123456,
+      salt: new TextEncoder().encode(salt),
+    };
+    let af = await substl.deriveBits(pbkdf2, key, 256);
+    let arrPri = new Uint8Array(af);
 
-  
-  let pbkdf2  = {
-    name:"PBKDF2",
-    hash: "SHA-256",
-    iterations: 123456,
-    salt: new TextEncoder().encode(salt)
+    console.log(ec.base64Encode(arrPri));
+
+    let bf64 = ec.base64Encode(arrPri);
+    let kp = await ec.generateNewKeyPair(bf64);
+
+    return kp;
   }
-  let af = await substl.deriveBits(pbkdf2,key,256);
-  let arrPri = new Uint8Array(af);
-
-  console.log(ec.base64Encode(arrPri))
-
-  let bf64 = ec.base64Encode(arrPri);
-  let kp =  await ec.generateNewKeyPair(bf64)
-
-  return kp
-
-}
 
   document.getElementById("genkeyfrompharse")!.onclick = async () => {
-
     let input = document.getElementById("keyphrase") as HTMLInputElement;
-    let phrase = input?.value.trim(); 
+    let phrase = input?.value.trim();
     if (!phrase) {
-      setErrMsg('请输入密码短语')
-      return
+      setErrMsg("请输入密码短语");
+      return;
     }
 
     if (G_Input?.prefix) {
-      phrase = `${G_Input.prefix}${phrase}`
+      phrase = `${G_Input.prefix}${phrase}`;
     }
 
-    
-    
-    let kp = await pbkdf2(phrase)
+    let kp = await pbkdf2(phrase);
     setPirvateKey(kp.private);
     setPublicKey(kp.public);
   };
 
   document.getElementById("downloadPlain")!.onclick = async () => {
     let s = getPlainText();
-    if(!s){
-      setErrMsg("文件内容为空")
-      return
+    if (!s) {
+      setErrMsg("文件内容为空");
+      return;
     }
     let te = new TextEncoder();
     let blob = new Blob([te.encode(s)]);
-    const fileName = `dec_${(filename())}.txt`;
+    const fileName = `dec_${filename()}.txt`;
     const link = document.createElement("a");
     link.href = window.URL.createObjectURL(blob);
     link.download = fileName;
@@ -207,23 +202,24 @@
     window.URL.revokeObjectURL(link.href);
   };
 
-  function beijingtime(){
-    return new Date(Date.now() + 8 * 3600000).toISOString()
-    .replace("T",' ')
-    .replace("Z",'') + ' +0800'
+  function beijingtime() {
+    return (
+      new Date(Date.now() + 8 * 3600000)
+        .toISOString()
+        .replace("T", " ")
+        .replace("Z", "") + " +0800"
+    );
   }
 
-  function filename(){
-    return beijingtime()
-    .replace(/:/g,'_')
-    .substring(0,19) 
+  function filename() {
+    return beijingtime().replace(/:/g, "_").substring(0, 19);
   }
 
   document.getElementById("downloadCipher")!.onclick = async () => {
     let s = getCipherText();
-    if(!s){
-      setErrMsg("文件内容为空")
-      return
+    if (!s) {
+      setErrMsg("文件内容为空");
+      return;
     }
     let cipher = ec.base64Decode(s);
     let blob = new Blob([cipher]);
@@ -239,24 +235,53 @@
     let obj = document.getElementById("clearfileform") as HTMLFormElement;
     obj.reset();
   };
-  document.getElementById("sendemail")!.onclick = async () => {
-
-   let cipher = getCipherText()
-   if (!cipher) {
-    let t = await encryptClick();
-    if(!t){
-      return
-    }
-    cipher = getCipherText()
+  document.getElementById("sendemail2")!.onclick = async () => {
+    let cipher = getCipherText();
     if (!cipher) {
-      return
+      let t = await encryptClick();
+      if (!t) {
+        return;
+      }
+      cipher = getCipherText();
+      if (!cipher) {
+        return;
+      }
     }
-   }
+
+    let msg = `
+   ${G_Input?.prefix || ""}
+   备份时间:${beijingtime()}
+
+   公钥:${getPublicKey()}
+
+   网页地址:
+   ${location.href}
+
+   数据base64:
 
 
+   `;
+    let mailto = `mailto:${G_Input.toEmail}?subject=${encodeURIComponent(
+      G_Input.emailSubject || "备份"
+    )}&body=${encodeURIComponent(msg)}`;
+    console.log(mailto);
+    window.open(mailto, "target", "");
+  };
+  document.getElementById("sendemail")!.onclick = async () => {
+    let cipher = getCipherText();
+    if (!cipher) {
+      let t = await encryptClick();
+      if (!t) {
+        return;
+      }
+      cipher = getCipherText();
+      if (!cipher) {
+        return;
+      }
+    }
 
-   let msg = `
-   ${G_Input?.prefix || ''}
+    let msg = `
+   ${G_Input?.prefix || ""}
    备份时间:${beijingtime()}
 
    公钥:${getPublicKey()}
@@ -271,112 +296,118 @@
 
 
 
-   `
-   let mailto = `mailto:${G_Input.toEmail}?subject=${encodeURIComponent(G_Input.emailSubject || "备份")}&body=${encodeURIComponent(msg)}`
-   console.log(mailto)
-   window.open(mailto,'target','');
-
+   `;
+    let mailto = `mailto:${G_Input.toEmail}?subject=${encodeURIComponent(
+      G_Input.emailSubject || "备份"
+    )}&body=${encodeURIComponent(msg)}`;
+    console.log(mailto);
+    window.open(mailto, "target", "");
   };
 
-  
-  let webPrivate = 'yNmVrcoS5D4xMTvjAPSkZe57HZqPZoIUxznm+SqWKFo='
-  let webPublic =  'dTj41nmwoLcguLpM9AntyKgg67xx6K4UAxc27CLIcFw='
+  let webPrivate = "yNmVrcoS5D4xMTvjAPSkZe57HZqPZoIUxznm+SqWKFo=";
+  let webPublic = "dTj41nmwoLcguLpM9AntyKgg67xx6K4UAxc27CLIcFw=";
 
-  async function genbookmark(pubkey:string,toEmail:string,prefix:string,emailSubject?:string){
-    let s = {prefix,pubkey,toEmail,emailSubject}
+  async function genbookmark(
+    pubkey: string,
+    toEmail: string,
+    prefix: string,
+    emailSubject?: string
+  ) {
+    let s = { prefix, pubkey, toEmail, emailSubject };
 
-    let jsonstring = JSON.stringify(s)
-    let arr = new TextEncoder().encode(jsonstring)
-    let dataBuff = await ec.encrypt(webPublic,arr);
-    let data = ec.base64Encode(dataBuff)
+    let jsonstring = JSON.stringify(s);
+    let arr = new TextEncoder().encode(jsonstring);
+    let dataBuff = await ec.encrypt(webPublic, arr);
+    let data = ec.base64Encode(dataBuff);
 
-    let bookmark = `${location.origin}${location.pathname }?t=${Date.now()}#&data=${encodeURIComponent(data)}`
+    let bookmark = `${location.origin}${
+      location.pathname
+    }?t=${Date.now()}#&data=${encodeURIComponent(data)}`;
 
-    let a = document.createElement("a")
-    a.innerText = bookmark
-    a.href = bookmark
+    let a = document.createElement("a");
+    a.innerText = bookmark;
+    a.href = bookmark;
 
-    let holder = document.getElementById('bookmark')
+    let holder = document.getElementById("bookmark");
     holder?.replaceChildren(a);
   }
 
   document.getElementById("genbookmark")!.onclick = async () => {
-
     let pubkey = getPublicKey();
     if (!pubkey) {
-      setErrMsg("公钥为空")
-      return
+      setErrMsg("公钥为空");
+      return;
     }
 
-    let prefixE = document.getElementById("prefix") as HTMLInputElement
-    let prefix =  prefixE.value.trim()
+    let prefixE = document.getElementById("prefix") as HTMLInputElement;
+    let prefix = prefixE.value.trim();
 
-    let emailEle = document.getElementById("email") as HTMLInputElement
-    let toEmail =  emailEle.value.trim()
+    let emailEle = document.getElementById("email") as HTMLInputElement;
+    let toEmail = emailEle.value.trim();
 
-    let emailSubjectEle = document.getElementById("emailsubject") as HTMLInputElement
-    let subject =  emailSubjectEle.value.trim()
-   
-    await genbookmark(pubkey,toEmail,prefix,subject)
+    let emailSubjectEle = document.getElementById(
+      "emailsubject"
+    ) as HTMLInputElement;
+    let subject = emailSubjectEle.value.trim();
 
+    await genbookmark(pubkey, toEmail, prefix, subject);
   };
 
   document.getElementById("genbookmark2")!.onclick = async () => {
-
     let input = document.getElementById("keyphrase") as HTMLInputElement;
-    let phrase = input?.value.trim(); 
+    let phrase = input?.value.trim();
     if (!phrase) {
-      setErrMsg('密码短语为空')
-      return
+      setErrMsg("密码短语为空");
+      return;
     }
 
-    let prefixE = document.getElementById("prefix") as HTMLInputElement
-    let prefix =  prefixE.value.trim()
+    let prefixE = document.getElementById("prefix") as HTMLInputElement;
+    let prefix = prefixE.value.trim();
 
-    let phrase2 = `${prefix || ""}${phrase||""}`
+    let phrase2 = `${prefix || ""}${phrase || ""}`;
 
-    let pubkey  = (await pbkdf2(phrase2)).public;
+    let pubkey = (await pbkdf2(phrase2)).public;
 
-    let emailEle = document.getElementById("email") as HTMLInputElement
-    let toEmail =  emailEle.value.trim()
+    let emailEle = document.getElementById("email") as HTMLInputElement;
+    let toEmail = emailEle.value.trim();
 
-    let emailSubjectEle = document.getElementById("emailsubject") as HTMLInputElement
-    let subject =  emailSubjectEle.value.trim()
-   
-    await genbookmark(pubkey,toEmail,prefix,subject)
+    let emailSubjectEle = document.getElementById(
+      "emailsubject"
+    ) as HTMLInputElement;
+    let subject = emailSubjectEle.value.trim();
 
+    await genbookmark(pubkey, toEmail, prefix, subject);
   };
 
-  
+  let btime = document.getElementById("build") as HTMLElement;
 
-  
-  let btime = document.getElementById('build') as HTMLElement
-  
-  btime.innerText =`编译信息:\n${__BUILD_MOD__}\n${__BUILD_TIME__} ` ;
+  btime.innerText = `编译信息:\n${__BUILD_MOD__}\n${__BUILD_TIME__} `;
 
+  (async function initDefaultValues() {
+    console.log(location.hash);
+    let search = new URLSearchParams(location.hash);
 
-  (async function initDefaultValues(){
-    console.log(location.hash)
-    let search = new URLSearchParams(location.hash)
-     
-    let data = search.get("data") as string
-
+    let data = search.get("data") as string;
 
     let ttlog = console.log;
-    ttlog({webPrivate,webPublic})
+    ttlog({ webPrivate, webPublic });
 
     let plainBf = await ec.decrypt(webPrivate, ec.base64Decode(data));
-    let plain = new TextDecoder().decode(plainBf)
-    ttlog(plain)
+    let plain = new TextDecoder().decode(plainBf);
+    ttlog(plain);
 
-    let jsonObj = JSON.parse(plain) as  InputData
-    
+    let jsonObj = JSON.parse(plain) as InputData;
+
     if (jsonObj) {
-      G_Input = jsonObj
-      let inputDataElement =  document.getElementById('inputData')!
-      inputDataElement.innerText = `传入参数:\n ${JSON.stringify(G_Input,null,'\t')}`
+      G_Input = jsonObj;
+      let inputDataElement = document.getElementById("inputData")!;
+      inputDataElement.innerText = `传入参数:\n ${JSON.stringify(
+        G_Input,
+        null,
+        "\t"
+      )}`;
       setPublicKey(jsonObj.pubkey);
-    }else{      
+    } else {
     }
-  })()
+  })();
 })();
